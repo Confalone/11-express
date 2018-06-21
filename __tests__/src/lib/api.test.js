612ba80ba9cfdaba44ad797b2547b54335e7bcef
Tyler Confalone <tconfalone@gmail.com>
2018-06-20 20:15:06 -0700

    added server beforeall and afterall methods in test

--- __tests__/src/lib/api.test.js
+++ __tests__/src/lib/api.test.js
@@ -2,14 +2,14 @@
 
 const superagent = require('superagent');
 // const api = require('../../../src/api/api.js');
-// const app = require('../../../src/app.js');
+const app = require('../../../src/app.js');
 
 const apiURL = 'http://localhost:3000/api/v1/Tylers';
 
 describe('/api/v1/Tylers', () => {
 
-  // beforeAll(app.start);
-  // afterAll(app.stop);
+  beforeAll(app.start);
+  afterAll(app.stop);
   
   it('testing that api/v1/Tylers GET one by id route returns a 200 status code on success', () => {
     return superagent.get(`${apiURL}/7d418060-7376-11e8-b3e4-7b5560d0b48c`)
